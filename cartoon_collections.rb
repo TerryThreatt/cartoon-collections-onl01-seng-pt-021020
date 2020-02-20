49830f60ea92d1363a644d8598349129de2b93a9
Terry Threatt <terry.threatt@gmail.com>
2020-02-20 13:10:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,4 @@
 def roll_call_dwarves(array)
-  array.each_with_index do {|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
@@ -13,4 +12,7 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include?(item)
+  end
 end
