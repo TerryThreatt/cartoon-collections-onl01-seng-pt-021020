016f65c0204a3d59a692ab63b3b05e97f853e07b
Terry Threatt <terry.threatt@gmail.com>
2020-02-20 13:20:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(array)
-  array.collect do |dwarf, i|
-    i = 1 
-    i += 1 
-    puts "#{i}. #{dwarf}"
-  end 
+  i = 0 
+  while i < array.length do |i|
+    puts "#{i + 1}. #{array[0]}"
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
