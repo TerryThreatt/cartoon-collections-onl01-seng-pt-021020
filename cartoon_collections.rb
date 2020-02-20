6fef11fea23fdb11107643d1cde5719193417a71
Terry Threatt <terry.threatt@gmail.com>
2020-02-20 12:46:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  i = 0 
+  While i < array.length
+    yield(array[i])
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
